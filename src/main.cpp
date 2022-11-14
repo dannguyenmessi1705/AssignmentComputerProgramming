92757f924c8f7ef53eb5e8efae35cf4fa64baa9f
dannguyenmessi1705 <messiprohy@gmail.com>
2022-11-15 02:33:14 +0700

    Uploaded by Nguyễn Di Đan

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "bits\Customer.h"
+int main(){
+    Login();
+    TimDoDai(lenid, lenname, lenaddress, lenemail, lenphone, lenrate, lenbill);
+    LIST l = LIST();
+    system("Color 70");
+    MENU(l);
+    l.ClearList();
+}
